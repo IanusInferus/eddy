8fc7e4627e38bcc17ab2546d0cf7e9ccd100eefb
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2010-12-31 08:34:11 +0000

    Wpf: 增加WPF界面中。

--- Src/Wpf/WindowMain.xaml.vb
+++ Src/Wpf/WindowMain.xaml.vb
@@ -1,16 +1,3 @@
-﻿'==========================================================================
-'
-'  File:        WindowMain.vb
-'  Location:    Eddy <Visual Basic .Net>
-'  Description: 主窗体
-'  Version:     2010.12.31.
-'  Copyright(C) F.R.C.
-'
-'==========================================================================
-
-Imports System
-Imports System.Collections.Generic
-
-Partial Public NotInheritable Class WindowMain
+﻿Public Class WindowMain2
 
 End Class
